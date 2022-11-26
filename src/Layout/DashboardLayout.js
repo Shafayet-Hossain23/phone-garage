bf14f6ff1540107a1aa75d9e53454c2cc016e93f
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-11-27 00:40:43 +0600

    seller-admin-dashboard

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Toaster } from 'react-hot-toast';
 import Header from '../sharedPages/Header/Header';
 import { Link, Outlet } from 'react-router-dom';
+import useAdmin from '../Components/useAdmin';
+import { AuthContext } from '../ContextApi/UserContext';
+import useSeller from '../Components/useSeller';
 
 
 const DashboardLayout = () => {
+    const { user } = useContext(AuthContext)
+    const [isAdmin] = useAdmin(user?.email)
+    const [isSeller] = useSeller(user?.email)
     return (
         <div className='lg:mx-14 mx-5'>
             <Header></Header>
@@ -20,6 +26,22 @@ const DashboardLayout = () => {
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-60 bg-base-100 text-base-content">
                         <li className='text-xl font-semibold'><Link to='/dashboard'>My Orders</Link></li>
+                        {
+                            isSeller && <>
+                                <li className='text-xl font-semibold'><Link to='/dashboard'>Add A Products</Link></li>
+                                <li className='text-xl font-semibold'><Link to='/dashboard'>My Products</Link></li>
+                                <li className='text-xl font-semibold'><Link to='/dashboard'>My Buyers</Link></li>
+                            </>
+
+                        }
+                        {
+                            isAdmin && <>
+                                <li className='text-xl font-semibold'><Link to='/dashboard'>All Sellers</Link></li>
+                                <li className='text-xl font-semibold'><Link to='/dashboard'>All Buyers</Link></li>
+                                <li className='text-xl font-semibold'><Link to='/dashboard'>Reported Items</Link></li>
+                            </>
+
+                        }
 
                     </ul>
 
